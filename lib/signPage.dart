e1e9977a99791d262b7bc02cb1f53d8d75f1f7bd
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-02-23 20:47:46 +0900

    20230223 회원가입 페이지

--- lib/signPage.dart
+++ lib/signPage.dart
@@ -32,29 +32,101 @@ class _SignPage extends State<SignPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('회원가입'),
-      ),
-      body: Container(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                width: 200,
-                child: TextField(
-                  controller: _idTextController,
-                  maxLines: 1,
-                  decoration: InputDecoration(
+        appBar: AppBar(
+          title: Text('회원가입'),
+        ),
+        body: Container(
+            child: Center(
+                child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 200,
+              child: TextField(
+                controller: _idTextController,
+                maxLines: 1,
+                decoration: InputDecoration(
                     hintText: '4자 이상 입력해주세요',
                     labelText: '아이디',
-                    border: OutlineInputBorder()
-                  ),
-                ),
+                    border: OutlineInputBorder()),
               ),
-            ],
-          )
-        )
-      )
-    );
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 200,
+              child: TextField(
+                controller: _pwTextController,
+                obscureText: true,
+                maxLines: 1,
+                decoration: InputDecoration(
+                    hintText: '6자 이상 입력해주세요',
+                    labelText: '비밀번호',
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 200,
+              child: TextField(
+                controller: _pwCheckController,
+                obscureText: true,
+                maxLines: 1,
+                decoration: InputDecoration(
+                    labelText: '비밀번호학인', border: OutlineInputBorder()),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_idTextController!.value.text.length >= 4 &&
+                    _pwTextController!.value.text.length >= 6) {
+                  if (_pwTextController!.value.text ==
+                      _pwCheckController!.value.text) {
+                    var bytes = utf8.encode(_pwTextController!.value.text);
+                    var digest = sha1.convert(bytes);
+                    reference!
+                        .child(_idTextController!.value.text)
+                        .push()
+                        .set(User(
+                                _idTextController!.value.text,
+                                digest.toString(),
+                                DateTime.now().toIso8601String())
+                            .toJson())
+                        .then((_) {
+                      Navigator.of(context).pop();
+                    });
+                  } else {
+                    makeDialog('비밀번호가 틀립니다');
+                  }
+                } else {
+                  makeDialog('길이가 짧습니다');
+                }
+              },
+              child: Text(
+                '회원가입',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.blueAccent)),
+            )
+          ],
+        ))));
+  }
+
+  void makeDialog(String text) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(text),
+          );
+        });
   }
 }
