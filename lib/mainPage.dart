e790d60fee970f0574ac4b888c005bb82c5e6ba9
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-03-03 21:29:21 +0900

    20230303

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -3,3 +3,22 @@ import 'package:flutter/material.dart';
 import 'main/favoritePage.dart';
 import 'main/settingPage.dart';
 import 'main/mapPage.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MainPage();
+}
+
+class _MainPage extends State<MainPage> with SingleTickerProviderStateMixin {
+  TabController? controller;
+  FirebaseDatabase? _database;
+  DatabaseReference? reference;
+  String _databaseURL = '';
+  String? id;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
