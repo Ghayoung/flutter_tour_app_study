0b5daa8de3d4bc32b6967ade3a71c67659f40e1f
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-03-04 20:05:38 +0900

    20230304 메인페이지

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -17,8 +17,47 @@ class _MainPage extends State<MainPage> with SingleTickerProviderStateMixin {
   String? id;
 
   @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 3, vsync: this);
+    _database = FirebaseDatabase(databaseURL: _databaseURL);
+    reference = _database!.reference();
+  }
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    id = ModalRoute.of(context)!.settings.arguments as String?; // 로그인할 때 전달받은 id값
+    return Scaffold(
+      body: TabBarView(
+        controller: controller,
+        children: <Widget>[
+          MapPage(), // 관광 정보 표시
+          FavoritePage(), // 즐겨찾기 페이지
+          SettingPage() // 설정 페이지
+        ],
+      ),
+      bottomNavigationBar: TabBar(
+        tabs: <Tab>[
+          Tab(
+            icon: Icon(Icons.map),
+          ),
+          Tab(
+            icon: Icon(Icons.star),
+          ),
+          Tab(
+            icon: Icon(Icons.settings),
+          )
+        ],
+        labelColor: Colors.amber,
+        indicatorColor: Colors.deepOrangeAccent,
+        controller: controller,
+      )
+    );
   }
 }
