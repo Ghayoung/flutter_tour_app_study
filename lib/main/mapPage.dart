6297aa7eaf905597754a61a623d27edfb5865762
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-03-08 23:43:05 +0900

    20230308 listData

--- lib/main/mapPage.dart
+++ lib/main/mapPage.dart
@@ -6,13 +6,15 @@ import 'package:tour_app_practice/data/tour.dart';
 //import 'package:tour_app_practice/data/listData.dart';
 import 'package:sqflite/sqflite.dart';
 
-
-
 class MapPage extends StatefulWidget {
+  final DatabaseReference? databaseReference;
+  final Future<Database>? db;
+  final String? id;
+
+  MapPage({this.databaseReference, this.db, this.id});
+
   @override
-  State<StatefulWidget> createState() {
-    return _MapPage();
-  }
+  State<StatefulWidget> createState() => _MapPage();
 }
 
 class _MapPage extends State<MapPage> {
