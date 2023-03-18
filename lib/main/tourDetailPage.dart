56863c5dcf011f601d01aa81f31a3f9c60fcdc6c
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-03-18 15:35:39 +0900

    20230318 중간커밋

--- lib/main/tourDetailPage.dart
+++ lib/main/tourDetailPage.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:math' as math;
 import 'package:tour_app_practice/data/tour.dart';
+import 'package:tour_app_practice/data/disableInfo.dart';
+import 'package:tour_app_practice/data/reviews.dart';
 
 class TourDetailPage extends StatefulWidget {
   final TourData? tourData;
@@ -19,6 +21,17 @@ class TourDetailPage extends StatefulWidget {
 }
 
 class _TourDetailPage extends State<TourDetailPage> {
+  Completer<GoogleMapController> _controller = Completer();
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  CameraPosition? _googleMapCamera;
+  TextEditingController? _reviewTextController;
+  Marker? marker;
+  List<Review> reviews = List.empty(growable: true);
+  bool _disableWidget = false;
+  DisableInfo? _disableInfo;
+  double disableCheck1 = 0;
+  double disableCheck2 = 0;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
