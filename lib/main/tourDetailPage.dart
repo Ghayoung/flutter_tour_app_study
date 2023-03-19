ebbd1ac6be0384d5a906601141e796c6c6a341d3
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-03-19 22:16:50 +0900

    20230319 중간커밋

--- lib/main/tourDetailPage.dart
+++ lib/main/tourDetailPage.dart
@@ -33,8 +33,60 @@ class _TourDetailPage extends State<TourDetailPage> {
   double disableCheck2 = 0;
 
   @override
+  void initState() {
+    super.initState();
+    widget.databaseReference!
+        .child('tour')
+        .child(widget.tourData!.id.toStirng())
+        .child('review')
+        .onChildAdded
+        .listen((event) {
+      if (event.snapshot.value != null) {
+        setState(() {
+          reviews.add(Review.fromSnapshot(event.snapshot));
+        });
+      }
+    });
+
+    _reviewTextController = TextEditingController();
+    // 지도에서 관광지 위치를 표시하기 위한 변수
+    _googleMapCamera = CameraPosition(
+      target: LatLng(double.parse(widget.tourData!.mapy.toString()),
+          double.parse(widget.tourData!.mapx.toString())),
+      zoom: 16,
+    );
+    MarkerId markerId = MarkerId(widget.tourData!.mapy.toString());
+    marker = Marker(
+        position: LatLng(double.parse(widget.tourData!.mapy.toString()),
+            double.parse(widget.tourData!.mapx.toString())),
+        flat: true,
+        markerId: markerId);
+    markers[markerId] = marker!;
+    getDisableInfo();
+  }
+
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     throw UnimplementedError();
+  }
+
+  getDisableInfo() {
+    widget.databaseReference!
+        .child('tour')
+        .child(widget.tourData!.id.toString())
+        .onValue
+        .listen((event) {
+      _disableInfo = DisableInfo.fromSnapshot(event.snapshot);
+      if (_disableInfo!.id == null) {
+        setState(() {
+          _disableWidget = false;
+        });
+      } else {
+        setState(() {
+          _disableWidget = true;
+        });
+      }
+    });
   }
 }
