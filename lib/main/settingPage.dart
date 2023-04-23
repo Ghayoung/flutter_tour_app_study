4729d73a45702960b3bee01bcfbc2ed494b65445
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-04-23 23:49:34 +0900

    20230423

--- lib/main/settingPage.dart
+++ lib/main/settingPage.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class SettingPage extends StatefulWidget {
+  final DatabaseReference? databaseReference;
+  final String? id;
+
+  SettingPage({this.databaseReference, this.id});
+
   @override
   State<StatefulWidget> createState() {
     return _SettingPage();
@@ -8,8 +14,20 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPage extends State<SettingPage> {
+  bool pushCheck = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
+  }
+
+  void _loadData() async {
+    var key = "push";
   }
 }
